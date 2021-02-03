f145adf2764e229ff51359d1643214c0b30447f3
Andrii Korutchak <77465360+mandkorb@users.noreply.github.com>
2021-02-03 22:12:03 +0200

    Add final UI

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ from tkinter import filedialog, Text
 
 root = tk.Tk()
 root.title("Workspace Opener")
-root.iconbitmap("chief.png")
-root.geometry("450x300")
+root.geometry("500x300")
 root.resizable(width=False, height=False)
 
-canvas = tk.Canvas(root, height=300, width=450, bg="#fafafa")
-canvas.pack()
+canvas = tk.Canvas(root, width=500, height=300, bg="#2E3348").pack()
 
-frame = tk.Frame(root, bg="#2E3348")
-frame.place(relwidth=0.2, relheight=1)
+apps_frame = tk.Frame(root, bg="#fafafa")
+apps_frame.place(relwidth=0.8, relheight=0.92, relx=0.175, rely=0.04)
 
+buttons_frame = tk.Frame(root, bg="#2E3348")
+buttons_frame.place(relwidth=0.17, relheight=0.95, relx=0.01, rely=0.04)
+
+open_file = tk.Button(buttons_frame, text="Open File", padx=5, pady=5,
+                    fg="#2E3348", bg="#fafafa", font="helvetica 10").pack()
+
+run_apps = tk.Button(buttons_frame, text="Run Apps", padx=4, pady=5,
+                    fg="#2E3348", bg="#fafafa", font="helvetica 10").pack(pady=10)
 
 root.mainloop()
