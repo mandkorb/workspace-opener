5d55520a96d8f0e4f53202fe2fe067d0026bfe78
Andrii Korutchak <77465360+mandkorb@users.noreply.github.com>
2021-02-03 23:27:12 +0200

    Add functional buttons Open File and Run Apps

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ root.title("Workspace Opener")
 root.geometry("500x300")
 root.resizable(width=False, height=False)
 
+apps = []
+
+def add_app():
+    for app in apps_frame.winfo_children():
+        app.destroy()
+
+    filename = filedialog.askopenfilename(initialdir="/", title="Select App",
+                    filetypes=(("apps", "*.exe"), ("*all files", ".")))   
+    apps.append(filename)
+
+    for app in apps:
+        app_path_label = tk.Label(apps_frame, text=app).pack(pady=5)
+    
+def run_apps():
+    for app in apps:
+        os.startfile(app)
+
 canvas = tk.Canvas(root, width=500, height=300, bg="#2E3348").pack()
 
 apps_frame = tk.Frame(root, bg="#fafafa")
@@ -16,9 +33,9 @@ buttons_frame = tk.Frame(root, bg="#2E3348")
 buttons_frame.place(relwidth=0.17, relheight=0.95, relx=0.01, rely=0.04)
 
 open_file = tk.Button(buttons_frame, text="Open File", padx=5, pady=5,
-                    fg="#2E3348", bg="#fafafa", font="helvetica 10").pack()
+            fg="#2E3348", bg="#fafafa", font="helvetica 10", command=add_app).pack()
 
 run_apps = tk.Button(buttons_frame, text="Run Apps", padx=4, pady=5,
-                    fg="#2E3348", bg="#fafafa", font="helvetica 10").pack(pady=10)
+            fg="#2E3348", bg="#fafafa", font="helvetica 10", command=run_apps).pack(pady=10)
 
 root.mainloop()
