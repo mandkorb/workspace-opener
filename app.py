f375f0cac4adbeee1948e8e27a9bea3bafc386fb
Andrii Korutchak <77465360+mandkorb@users.noreply.github.com>
2021-02-04 09:47:54 +0200

    Add Open rom .txt and Save buttons

--- app.py
+++ app.py
@@ -24,6 +24,26 @@ def run_apps():
     for app in apps:
         os.startfile(app)
 
+def open_from_txt():
+    apps_in_file = filedialog.askopenfilename(initialdir="/", title="Select FIle",
+                    filetypes=(("text", "*.txt"), ("*all files", ".")))
+
+    with open(apps_in_file, "r") as f:
+        for path in f:
+            temp_apps = [elt.strip() for elt in path.split(',')]
+            
+    for item in temp_apps:
+        apps.append(item)
+    
+    for app in apps:
+        app_path_label = tk.Label(apps_frame, text=app).pack(pady=5)     
+
+def save_to_file():
+    if len(apps) != 0:
+        with open("template.txt", "w") as f:
+            for app in apps:
+                f.write(app + ",")
+            
 canvas = tk.Canvas(root, width=500, height=300, bg="#2E3348").pack()
 
 apps_frame = tk.Frame(root, bg="#fafafa")
@@ -37,5 +57,12 @@ open_file = tk.Button(buttons_frame, text="Open File", padx=5, pady=5,
 
 run_apps = tk.Button(buttons_frame, text="Run Apps", padx=4, pady=5,
             fg="#2E3348", bg="#fafafa", font="helvetica 10", command=run_apps).pack(pady=10)
+
+open_saved = tk.Button(buttons_frame, text="Open .txt", padx=5.5, pady=5,
+            fg="#2E3348", bg="#fafafa", font="helvetica 10", command=open_from_txt).pack()
+
+save = tk.Button(buttons_frame, text="Save", padx=18, pady=5,
+            fg="#2E3348", bg="#fafafa", font="helvetica 10", command=save_to_file).pack(pady=10)
+
 
 root.mainloop()
